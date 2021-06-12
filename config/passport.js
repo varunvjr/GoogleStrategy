15b95f4433a264b06ab9d0f629a688db157c9f58
Varun Jaisundar Raju <53809340+varunvjr@users.noreply.github.com>
2021-06-12 08:32:30 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,8 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports=function(passport){
     passport.use(new GoogleStrategy({
-    clientID: "819456955728-mlubj1d3pfljjjeotj8la2voabehohi6.apps.googleusercontent.com",
-    clientSecret: "U7LRCW57AWMvcPAgr5KC7it3",
+    clientID: "************",
+    client",
     callbackURL: "http://localhost:8000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
